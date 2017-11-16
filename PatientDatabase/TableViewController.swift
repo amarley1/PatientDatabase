04bef60fab4377519c90722c447411e8ab5b4bcf
Allison Marley <allisonmarley@allisons-air-2.mobile.asu.edu>
2017-11-15 18:02:34 -0700

    Framework

---
+++ PatientDatabase/TableViewController.swift
@@ -0,0 +1,148 @@
+//
+//  TableViewController.swift
+//  PatientDatabase
+//
+//  Created by Allison Marley on 11/15/17.
+//  Copyright © 2017 ASU. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var patients: [Patient] = []
+    var selectedIndex: Int!
+    var savedPatient: [Patient] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        fetchData()
+    }
+    
+    
+    func fetchData() {
+        
+        do {
+            
+            patients = try context.fetch(Patient.fetchRequest())
+            savedPatient = patients
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
+            
+        } catch {
+            print("Couldn't Fetch Data")
+        }
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+        
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return savedPatient.count
+        
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = savedPatient[indexPath.row].name
+        
+        return cell
+    }
+   
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        //sets up delete button action
+        let delete = UITableViewRowAction(style: .default, title: "Delete") { (action, indexPath) in
+            // delete item at indexPath
+            
+            //selects specific note for deletion
+            let patient = self.savedPatient[indexPath.row]
+            self.context.delete(patient)
+            
+            //updates delegate
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            self.savedPatient.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        }
+        
+        return [delete]
+        
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ addPatientViewController.swift
@@ -0,0 +1,51 @@
+//
+//  addPatientViewController.swift
+//  PatientDatabase
+//
+//  Created by Allison Marley on 11/15/17.
+//  Copyright © 2017 ASU. All rights reserved.
+//
+import UIKit
+
+class addPatientViewController: UIViewController, UITextViewDelegate {
+    
+
+    
+    @IBOutlet var enterPatient: UITextView?
+
+    
+    @IBAction func savePatient(_ sender: Any) {
+    
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let newPatient = Patient(context: context)
+        newPatient.name =  enterPatient?.text!
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        enterPatient?.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+        
+    }
+    
+    
+}
